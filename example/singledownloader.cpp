9e3c1eabcd6dba5c9b08ddf4999695f5785b83c2
肖迎春 <1923536264@qq.com>
2026-03-21 21:47:19 +0800

    fix: singledownloader.cpp add some nicer code.

--- example/singledownloader.cpp
+++ example/singledownloader.cpp
@@ -20,38 +20,40 @@
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, 
                     void* userp) {
                     
-                    size_t totalSize = size * nmemb;
-                    
-                    std::ofstream *outFile = static_cast<std::ofstream*>(userp);
-                    if (outFile && outFile->is_open()) {
-                        outFile->write(static_cast<char*>(contents), totalSize);
+    size_t totalSize = size * nmemb;
+    
+    std::ofstream *outFile = static_cast<std::ofstream*>(userp);
+    if (outFile && outFile->is_open()) {
+        outFile->write(static_cast<char*>(contents), totalSize);
 
-                        if (outFile->bad()) {
-                            return 0;
-                        }
-                    } else {
-                        std::cerr << "WriteCallback: OutFile Doesn't Exist Or Can't Open!" << std::endl;
-                    }
-                    
-                    return totalSize;
+        if (outFile->bad()) {
+            std::cerr << "WriteCallback: Outfile Write Failed!" << std::endl;
+            return 0;
+        }
+    } else {
+        std::cerr << "WriteCallback: OutFile Doesn't Exist Or Can't Open!" << std::endl;
+    }
+    
+    return totalSize;
 } 
 
-void Download(CURLcode result, std::string targetPath, std::string savePath) {
-    if (targetPath == "" || savePath == "") {
+bool Download(const std::string& targetURL, const std::string& savePath) {
+    if (targetURL.empty() || savePath.empty()) {
         std::cerr << "File Path Error: " << std::endl
-                  << "targetPath: " << targetPath
+                  << "targetPath: " << targetURL
                   << "savePath: " << savePath << std::endl;
-        return;
+        return false;
     }
 
     CURL *curl = curl_easy_init();
     if (curl) {
         // curl is writed for C language, so don't use C++ string
-        curl_easy_setopt(curl, CURLOPT_URL, targetPath.c_str());
+        curl_easy_setopt(curl, CURLOPT_URL, targetURL.c_str());
         
         std::ofstream saveFile(savePath, std::ios::binary | std::ios::trunc);
         if (!saveFile) {
             std::cerr << "SaveFile Creating Failed!" << std::endl;
+            return false;
         }
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &saveFile);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
@@ -59,14 +61,14 @@ void Download(CURLcode result, std::string targetPath, std::string savePath) {
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); 
         curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 5L);
 
-        result = curl_easy_perform(curl);
+        CURLcode result = curl_easy_perform(curl);
         if (result == CURLE_OK) {
             // Get the HTTP response code
             long responseCode = 0;
             curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &responseCode);
             if (responseCode == 200) {
                 // Print the response data
-                std::cout << "Response from " << targetPath << std::endl
+                std::cout << "Response from " << targetURL << std::endl
                           << "Response data, please click: " << savePath << std::endl;
             } else {
                 std::cerr << "Response error code: " << responseCode << std::endl;
@@ -74,25 +76,29 @@ void Download(CURLcode result, std::string targetPath, std::string savePath) {
         } else {
             std::cerr << "curl_easy_perform(curl) failed: " 
                       << curl_easy_strerror(result) << std::endl;
+            return false;
         }
 
         curl_easy_cleanup(curl);
     } else {
         std::cerr << "Download: curl_easy_init() failed" << std::endl;
+        return false;
     }
+
+    return true;
 }
 
 int main() {
-    CURLcode result = curl_global_init(CURL_GLOBAL_ALL);
-    if (result != CURLE_OK) {
+    CURLcode globalResult = curl_global_init(CURL_GLOBAL_ALL);
+    if (globalResult != CURLE_OK) {
         std::cerr << "Curl_global_init Failed: "
-                  << curl_easy_strerror(result) << std::endl;
-        return static_cast<int>(result);
+                  << curl_easy_strerror(globalResult) << std::endl;
+        return static_cast<int>(globalResult);
     }
 
-    std::string targetPath = "https://github.com";
+    std::string targetURL = "https://github.com";
     std::string savePath = "D:/MyFiles/UniversityFiles/CareerInformation/cpp-parallel-downloader/output/singledownloader.txt";
-    Download(result, targetPath, savePath);
+    Download(targetURL, savePath);
 
     curl_global_cleanup();
 
